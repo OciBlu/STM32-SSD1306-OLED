08ebdb9d6a3110714c36553365a6d2bf5a1b8f21
diystron <diystron@gmail.com>
2025-04-22 18:31:29 +0700

    update

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -81,7 +81,7 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-
+  HAL_Delay(100); // tambahkan delay untuk auto reset supaya bisa auto tun ketika power on
   /* USER CODE END Init */
 
   /* Configure the system clock */
